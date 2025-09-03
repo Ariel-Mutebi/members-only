d78c53790a0dd656f20dc2be314107ece41a073c
black-RAM <pplanes311@gmail.com>
2025-09-03 06:39:02 +0300

    ssl for database client

--- db/singletons/config.ts
+++ db/singletons/config.ts
@@ -2,7 +2,8 @@ import { PoolConfig } from "npm:pg";
 import 'jsr:@std/dotenv/load';
 
 const config: PoolConfig = {
-  connectionString: Deno.env.get("PG_CONNECTION_STRING")
+  connectionString: Deno.env.get("PG_CONNECTION_STRING"),
+  ssl: { rejectUnauthorized: false }
 };
 
 export default config;
